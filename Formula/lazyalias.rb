ca04ec183043d5d1de5831dd7c0c1da752ecf465
Sergio Rivas <sergiorivas@gmail.com>
2025-02-20 17:22:08 -0500

    Add lazyalias formula

---
+++ Formula/lazyalias.rb
@@ -0,0 +1,12 @@
+class Lazyalias < Formula
+  desc "LazyAlias is a command-line tool that helps you manage and execute frequently used commands across different projects. It provides an interactive menu to select commands defined in a YAML configuration file and copies them to your clipboard for easy execution."
+  homepage "https://github.com/sergiorivas/lazyalias"
+  url "https://github.com/sergiorivas/lazyalias/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "661748f96b4f612b923ff17ade92e3ab1de3634f125b15ad8a9a32e07fd0e57c"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"lazyalias", "cmd/lazyalias"
+  end
+end
