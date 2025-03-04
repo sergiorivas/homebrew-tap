cdf47dc55041985885c9f2bf1422843ba53dd922
Sergio Rivas <sergiorivas@gmail.com>
2025-03-04 06:32:40 -0500

    chore: updating lazyalias v0.1.7

--- Formula/lazyalias.rb
+++ Formula/lazyalias.rb
@@ -1,8 +1,8 @@
 class Lazyalias < Formula
   desc "LazyAlias is a command-line tool that helps you manage and execute frequently used commands across different projects. It provides an interactive menu to select commands defined in a YAML configuration file and copies them to your clipboard for easy execution."
   homepage "https://github.com/sergiorivas/lazyalias"
-  url "https://github.com/sergiorivas/lazyalias/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "bac0bcdf0d4ad2c8e98b4acf1faff4d380956c6465df50bed813ffc858447196"
+  url "https://github.com/sergiorivas/lazyalias/archive/refs/tags/v0.1.7.tar.gz"
+  sha256 "78537960688144b9f9c105b18b2be0cec1bd5af9ba398219e4fb781c41d48045"
 
   depends_on "go" => :build
 
